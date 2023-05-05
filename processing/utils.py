0e1e64301521563398bbd8517ed32b509ec36587
jkdamewo <jkdamewo@mit.edu>
2023-05-04 20:25:32 -0400

    Started adding dataloaders

---
+++ processing/utils.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def filter_data_by_properties(df,props):
+    if isintance(props,str):
+        props = [props]
+    return df.dropna(subset=props)
+
+
+def select_structures(df,structure_type):
+    if structure_type == "unrelaxed":
+        df["structure"] = df['unrelaxed_struct']
+    else:
+        df["structure"] = df["opt_struct"]
+    return df
