85b86a786c34a569c89add3d1fc2de0f69ebb913
jkdamewo <jkdamewo@mit.edu>
2023-05-06 23:45:29 -0400

    Added Painn create model

---
+++ processing/create_model/create_painn_model.py
@@ -0,0 +1,34 @@
+from models.Perovskite-Ordering-CGNNS-painn.nff.train import get_model
+import numpy as np
+
+def get_painn_model(hyperparameters,train_loader,prop):
+
+    prop_name = prop+ "_diff"
+
+    training_labels = []
+    
+    for i, data in enumerate(train_loader):
+        
+        training_labels.append(data[prop_name])
+
+    training_data = np.concatenate(training_labels).ravel()
+
+    modelparams = {"feat_dim": 2**hyperparameters["log2_feat_dim"], 
+                   "activation": hyperparameters["activation"], 
+                   "n_rbf": 20,
+                   "cutoff": 5.0,
+                   "num_conv": hyperparameters["num_conv"], 
+                   "output_keys": [prop_name], 
+                   "grad_keys": [], 
+                   "skip_connection": {prop_name: False}, 
+                   "learnable_k": False, 
+                   "conv_dropout": 0.0, 
+                   "readout_dropout": 0.0, 
+                   "means": {prop_name: np.nanmean(training_data).item()}, 
+                   "stddevs": {prop_name: np.nanstd(training_data).item()}
+                  }
+
+        
+    model = get_model(modelparams, model_type="Painn")
+
+    return model, None
