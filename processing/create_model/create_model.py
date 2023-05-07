fa6220de8e9d85aba13aef59893739480fc543d8
jkdamewo <jkdamewo@mit.edu>
2023-05-06 23:45:29 -0400

    Added Painn create model

--- processing/create_model/create_model.py
+++ processing/create_model/create_model.py
@@ -3,12 +3,12 @@ from procssing.create_model.create_painn_model import get_painn_model
 from procssing.create_model.create_e3nn_model import get_e3nn_model
 
 
-def create_model(model_name, hyperparameters, train_loader):
+def create_model(model_name, hyperparameters, train_loader, prop="energy_above_hull"):
 
     if model_name == "CGCNN":
         return get_cgcnn_model(hyperparameters, train_loader)
     elif model_name == "Painn":
-        return get_painn_model(hyperparameters, train_loader)
+        return get_painn_model(hyperparameters, train_loader,prop)
     elif model_name == "e3nn":
         return get_e3nn_model(hyperparameters, train_loader)
     elif model_name == "e3nn_contrastive":
