12a9c3af019c007720d9a234f1f16caa0e015062
jkdamewo <jkdamewo@mit.edu>
2023-05-12 18:16:20 -0400

    Updated default hyperparameters

--- training/hyperparameters/default.py
+++ training/hyperparameters/default.py
@@ -2,6 +2,9 @@ def get_default_cgcnn_hyperparameters():
 
     hyperparameters = {}
 
+    hyperparameters['MaxEpochs'] = 100
+    hyperparameters["lr"] = 0.001
+    hyperparameters["reduceLR_patience"] = 10
     hyperparameters["atom_fea_len"] = 64
     hyperparameters["n_conv"] = 3
     hyperparameters["h_fea_len"] = 128
@@ -13,6 +16,9 @@ def get_default_painn_hyperparameters():
 
     hyperparameters = {}
 
+    hyperparameters['MaxEpochs'] = 100
+    hyperparameters["lr"] = 0.001
+    hyperparameters["reduceLR_patience"] = 10
     hyperparameters["log2_feat_dim"] = 6
     hyperparameters["activation"] = "ReLU"
     hyperparameters["num_conv"] = 3
@@ -24,6 +30,9 @@ def get_default_e3nn_hyperparameters():
 
     hyperparameters = {}
 
+    hyperparameters['MaxEpochs'] = 100
+    hyperparameters["lr"] = 0.001
+    hyperparameters["reduceLR_patience"] = 10
     hyperparameters['len_embedding_feature_vector'] = 64
     hyperparameters['num_hidden_feature'] = 128   
     hyperparameters['num_hidden_layer'] = 2
