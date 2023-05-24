1677b51532cb85b026a49657d8ef535ebd26e7b8
Jiayu Peng <jypeng@mit.edu>
2023-05-24 15:42:13 -0400

    Update preprocessing

--- processing/interpolation/Interpolation.py
+++ processing/interpolation/Interpolation.py
@@ -1,6 +1,5 @@
-import os
 import pandas as pd
-import json
+
 
 def update_pure(props):
     df = pd.read_json("data/edge_dataset.json")
@@ -16,48 +15,32 @@ def update_pure(props):
                         pure_dict_init[curr_perov] = [row[prop], row["dft_energy_per_atom"]]
                 else:
                     pure_dict_init[curr_perov] = [row[prop], row["dft_energy_per_atom"]]
+            else:
+                raise Warning("A non-binary composition exist in edge_dataset")
 
-        pure_dict = {}
-
+        Pure_ref = {}
         for key in pure_dict_init:
-            pure_dict[key[0] + '.' + key[1]] = pure_dict_init[key][0]
+            Pure_ref[key[0] + '.' + key[1]] = pure_dict_init[key][0]
 
-        WORKDIR = os.getcwd()
-        if not os.path.exists(WORKDIR + '/processing/interpolation/Pure_ref'):
-            os.makedirs(WORKDIR + '/processing/interpolation/Pure_ref')
+    return Pure_ref
 
-        with open(WORKDIR + "/processing/interpolation/Pure_ref/" + prop + ".json", "w") as outfile:
-            json.dump(pure_dict, outfile)
 
 def filter_alloys(df):
     filt = []
     for index, row in df.iterrows():
         if len(row["composition"]["sites"]["A"]) > 1 or len(row["composition"]["sites"]["B"]) > 1:
             filt.append(index)
+        else:
+             raise Warning("A binary composition exists in the examined dataset")
     if len(filt) == 0:
-        print("All Pure")
+        raise ValueError("All binary compositions")
         return None
     filt_structs = df.loc[filt]
+
     return filt_structs
 
-def filter_interpolation(df, prop, Interpolation, Difference):    
-    interp_out = []
-    diff_out = []
-    indexes = []
-    for i in range(len(Interpolation)):
-        interp_out.append(Interpolation[i])
-        diff_out.append(Difference[i])
-        indexes.append(i)  
-    df_out = df.iloc[indexes].copy()
-    df_out[prop + '_interp'] = interp_out
-    df_out[prop + '_diff'] = diff_out
-    return df_out, interp_out, diff_out
 
-def interpolator(df, prop):
-    WORKDIR = os.getcwd()
-    with open(WORKDIR + "/processing/interpolation/Pure_ref/" + prop + ".json") as json_file:
-        Pure_ref = json.load(json_file)
-
+def interpolator(df, prop, Pure_ref):
     Interpolation = []
     Difference = []
 
@@ -91,21 +74,37 @@ def interpolator(df, prop):
         else:
             curr_interpolation = None
 
+
         Interpolation.append(curr_interpolation)
         
         if curr_interpolation:
             Difference.append(row[prop] - curr_interpolation)
         else:
             Difference.append(None)
+            raise Warning("Cannot find all binary edges for an composition")
 
     return Interpolation, Difference
 
+
+def filter_interpolation(df, prop, Interpolation, Difference):    
+    interp_out = []
+    diff_out = []
+    indexes = []
+    for i in range(len(Interpolation)):
+        interp_out.append(Interpolation[i])
+        diff_out.append(Difference[i])
+        indexes.append(i)  
+    df_out = df.iloc[indexes].copy()
+    df_out[prop + '_interp'] = interp_out
+    df_out[prop + '_diff'] = diff_out
+    return df_out
+
+
 def apply_interpolation(df, prop):
-    update_pure([prop])
+    Pure_ref = update_pure([prop])
     df_interp = filter_alloys(df)
-    interp, diff = interpolator(df_interp, prop)
-    df_interp, interp_filter, diff_filter = filter_interpolation(df_interp, prop, interp, diff)
+    interp, diff = interpolator(df_interp, prop, Pure_ref)
+    df_interp = filter_interpolation(df_interp, prop, interp, diff)
     prop_diff_name = prop + "_diff"
-    prop_diff_names = [prop_diff_name]
     df_interp = df_interp.dropna(subset=[prop_diff_name], how='all')
     return df_interp
