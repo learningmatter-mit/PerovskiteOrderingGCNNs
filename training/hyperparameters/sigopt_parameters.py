c5d5ef5398efeb1a23850c2e7a3419c5f2661128
Jiayu Peng <jypeng@mit.edu>
2024-04-09 14:13:57 -0400

    Update painn hyperparam range

--- training/hyperparameters/sigopt_parameters.py
+++ training/hyperparameters/sigopt_parameters.py
@@ -18,7 +18,7 @@ def get_painn_hyperparameter_range():
     parameters=[
         # dict(name='MaxEpochs', bounds=dict(min=100, max=100), type="int"), 
         dict(name="batch_size", bounds=dict(min=4, max=16), type="int"),
-        dict(name="log_lr", bounds=dict(min=-5, max=-2), type="int"), 
+        dict(name="log_lr", bounds=dict(min=-5, max=-3), type="int"), 
         dict(name="reduceLR_patience", bounds=dict(min=10, max=30), type="int"), 
 
         dict(name="log2_feat_dim", bounds=dict(min=5, max=9), type="int"), 
